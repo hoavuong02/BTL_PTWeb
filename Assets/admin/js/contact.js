f7fbacdcffc4d5a23fb9ca29c1d6eae8f87929f5
unknown <damvietvuong123@gmail.com>
2022-04-02 23:42:00 +0700

    Chinh sua sidebar va table form

--- Assets/admin/js/contact.js
+++ Assets/admin/js/contact.js
@@ -1,23 +1,23 @@
 var modalDesc = document.querySelector(".modal-desc")
-        var actionPlus = document.querySelector(".action-plus")
-        var actionFix = document.querySelector(".action-fix")
-        var actionRemove = document.querySelector(".action-remove")
-        var modaldescSave = document.querySelector(".modaldesc__save")
-        var modaldescCancle = document.querySelector(".modaldesc__Cancle")
+var actionPlus = document.querySelector(".action-plus")
+var actionFix = document.querySelector(".action-fix")
+var actionRemove = document.querySelector(".action-remove")
+var modaldescSave = document.querySelector(".modaldesc__save")
+var modaldescCancle = document.querySelector(".modaldesc__Cancle")
 
 
-        actionPlus.addEventListener("click",function(){
-            modalDesc.style.display = "block"
-        })
-        actionFix.addEventListener("click",function(){
-            modalDesc.style.display = "block"
-        })
-        actionRemove.addEventListener("click",function(){
-            modalDelete.style.display = "block"
-        })
-        modaldescSave.addEventListener("click",function(){
-            modalDesc.style.display = "none"
-        })
-        modaldescCancle.addEventListener("click",function(){
-            modalDesc.style.display = "none"
-        })
+actionPlus.addEventListener("click", function () {
+    modalDesc.style.display = "block"
+})
+actionFix.addEventListener("click", function () {
+    modalDesc.style.display = "block"
+})
+actionRemove.addEventListener("click", function () {
+    modalDelete.style.display = "block"
+})
+modaldescSave.addEventListener("click", function () {
+    modalDesc.style.display = "none"
+})
+modaldescCancle.addEventListener("click", function () {
+    modalDesc.style.display = "none"
+})
